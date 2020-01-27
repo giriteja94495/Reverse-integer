03bb0ba4cdb26b58e8c747bf803693ae250bd647
GiriTejaBille <51397347+giriteja94495@users.noreply.github.com>
2020-01-27 23:10:18 +0530

    INTERVIEWBIT/REVERSE INTEGER
    Most asked interview question in many of the tier2 and tier3 companies ..this question includes the overflow conditions ...any suggestions to the code is considered.
    
    thank you:)

---
+++ ReverseInteger.cpp
@@ -0,0 +1,49 @@
+int Solution::reverse(int A) {
+   
+ if(A>INT_MAX||A<INT_MIN)
+{
+ 
+return 0;
+  
+  }
+   
+ long int reverse=0;
+
+    if(A<0){
+        
+A=-A;
+         
+while(A>0){
+
+        reverse=10*reverse+A%10;
+    
+    A=A/10;
+   
+ }
+     
+if(-(reverse)>INT_MAX||-(reverse)<INT_MIN){
+ 
+       return 0;
+    
+}
+    return -reverse;
+ 
+   }
+    while(A>0){
+    
+    reverse=10*reverse+A%10;
+  
+      A=A/10;
+   
+ }
+     
+if(reverse>INT_MAX||reverse<INT_MIN){
+   
+     return 0;
+    
+}
+    
+return reverse;
+  
+  }
+
